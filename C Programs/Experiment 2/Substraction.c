7aa9738e602437251e794b0fdf9d506ba3190be3
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-15 12:49:09 +0530

    Create Substraction.c

---
+++ C Programs/Experiment 2/Substraction.c
@@ -0,0 +1,17 @@
+// Write a C program for addition, substraction, multiplication and division of any numbers.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+int num1, num2, result;
+clrscr();
+printf("Enter first number:-");
+scanf("%d",&num1);
+printf("Enter second number:-");
+scanf("%d",&num2);
+result = num1-num2;
+printf("%d",result);
+getch();
+return 0;
+}
