9adf1169bb8da7f3b6d5fb190da20347d48cf347
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-22 09:44:41 +0530

    Create Program to print 3*3 matrix substraction.c

---
+++ C Programs/Experiment 17/Program to print 3*3 matrix substraction.c
@@ -0,0 +1,37 @@
+// Write a program to print 3*3 matrix substraction.
+
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+int arrA[3][3], arrB[3][3], arrC[3][3], i, j;
+clrscr();
+
+printf("Enter elements in array A:-");
+for (i = 0; i < 3; i++){
+for (j = 0; j < 3; j++){
+scanf ("%d",&arrA[i][j]);}
+}
+
+printf("Enter elements in array B:-");
+for (i = 0; i < 3; i++){
+for (j = 0; j < 3; j++){
+scanf("%d",&arrB[i][j]);}
+}
+
+for (i = 0; i < 3; i++){
+for (j = 0; j < 3; j++){
+arrC[i][j] = arrA[i][j] - arrB[i][j];}
+}
+
+for (i = 0; i < 3; i++){
+for (j = 0; j < 3; j++){
+printf("%d ",arrC[i][j]);}
+printf("\n");
+}
+
+getch();
+return 0;
+}
+
+
