9262a65f7b3d24d7c7191111ffe7281fdd44bea3
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-24 16:14:12 +0530

    Create Write a program that prints the string "The capital of France is Paris" in a new line and then prints the capital of your country in a new line.py

---
+++ Python/Write a program that prints the string "The capital of France is Paris" in a new line and then prints the capital of your country in a new line.py
@@ -0,0 +1,4 @@
+# Write a program that prints the string "The capital of France is Paris" in a new line and then prints the capital of your country in a new line.
+
+print("The capital of France is Paris")
+print("The capital of India is New Delhi")
