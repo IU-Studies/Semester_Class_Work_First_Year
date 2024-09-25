0e64279e5cd5d8f247e08ec7db3ed0d400fed03f
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-25 17:29:18 +0530

    Create Write a program that prompts the user to enter their favourite food and prints out a recipe for that food.py

---
+++ Python/Write a program that prompts the user to enter their favourite food and prints out a recipe for that food.py
@@ -0,0 +1,10 @@
+#Write a program that prompts the user to enter their favourite food and prints out a recipe for that food.
+
+user = input("Enter you favourite food like tea, coffee, maggie:-")
+if user == "tea":
+print("Heat water and add tea leafs and sugar and ginger after that add milk.")
+elif user == "coffee":
+print("Heat milk and add little bit of water and add sugar, pour some coffee powder in cup,
+and pour milk in cup")
+elif user == "maggie":
+print("Pour water into pan add maggie and masala")
