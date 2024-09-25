8ed0f01171fe09d7d4d2e2903c09d775652353a4
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-25 17:34:03 +0530

    Create Write a program that prompts the user to enter a number and prints out it's square root.py

---
+++ Python/Write a program that prompts the user to enter a number and prints out it's square root.py
@@ -0,0 +1,6 @@
+#Write a program that prompts the user to enter a number and prints out it's square root.
+
+import math
+user = int(input("Enter a number to get Sq. Root of it:-"))
+result = math.sqrt(user)
+print(result)
