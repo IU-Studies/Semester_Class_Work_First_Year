0e5c8473743e106ff32409f2bcb7037e80b1eadc
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-25 17:34:27 +0530

    Create Write a program that prompts the user to enter two numbers and prints out their sum, difference, product and quotient.py

---
+++ Python/Write a program that prompts the user to enter two numbers and prints out their sum, difference, product and quotient.py
@@ -0,0 +1,14 @@
+#Write a program that prompts the user to enter two numbers and prints out their sum, difference, product and quotient.
+
+num1 = float(input("Enter your first number:-"))
+num2 = float(input("Enter your second number:-"))
+
+rsum = num1 - num2
+rdifference = num1 - num2
+rproduct = num1*num2
+rquotient = num1//num2
+
+print("Sum of your numbers are",rsum)
+print("Difference of your numbers are",rdifference)
+print("Product of your numbers are",rproduct)
+print("Quotient of your numbers are",rquotient)
