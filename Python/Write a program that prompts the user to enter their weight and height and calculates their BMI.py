5d3296cfbb185beb6b462222ad5739883c4e20cd
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-25 17:28:24 +0530

    Create Write a program that prompts the user to enter their weight and height and calculates their BMI.py

---
+++ Python/Write a program that prompts the user to enter their weight and height and calculates their BMI.py
@@ -0,0 +1,5 @@
+#Write a program that prompts the user to enter their weight and height and calculates their BMI.
+weight = float(input("Enter your weight in Kilograms:-"))
+height = float(input("Enter your height in meters:-"))
+bmi = (weight)/(height*height)
+print(bmi)
