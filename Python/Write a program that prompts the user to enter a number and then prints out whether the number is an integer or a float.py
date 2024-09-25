506a20826bad607a60eb164ef09b8b48357fba33
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-25 17:37:13 +0530

    Create Write a program that prompts the user to enter a number and then prints out whether the number is an integer or a float.py

---
+++ Python/Write a program that prompts the user to enter a number and then prints out whether the number is an integer or a float.py
@@ -0,0 +1,4 @@
+#Write a program that prompts the user to enter a number and then prints out whether the number is an integer or a float.
+
+user = eval(input("Enter a number to check whether it's a integer or float:-"))
+print(type(user))
