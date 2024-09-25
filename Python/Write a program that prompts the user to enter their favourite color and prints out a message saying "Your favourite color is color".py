679cd4e8aecbc5c96f3aba63e03c3947a4d60112
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-25 17:28:55 +0530

    Create Write a program that prompts the user to enter their favourite color and prints out a message saying "Your favourite color is color".py

---
+++ Python/Write a program that prompts the user to enter their favourite color and prints out a message saying "Your favourite color is color".py
@@ -0,0 +1,3 @@
+#Write a program that prompts the user to enter their favourite color and prints out a message saying "Your favourite color is color".
+user = input("Enter you favourite colour:-")
+print("Your favourite colour is",user)
