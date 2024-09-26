878c095dc10b80e16067d70f43cb78947766ee27
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-26 18:52:56 +0530

    Create Write a program that prompts the user to enter a number and then prints out whether the number is positive or negative.py

---
+++ Python/Write a program that prompts the user to enter a number and then prints out whether the number is positive or negative.py
@@ -0,0 +1,7 @@
+#Write a program that prompts the user to enter a number and then prints out whether the number is positive or negative.
+
+user = float(input("Enter a number to check whether it is positive or Negative:-"))
+if user < 0:
+  print (user,"is Negative")
+else:
+  print (user,"is Positive")
