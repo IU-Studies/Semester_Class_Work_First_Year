abe213200c905b7fe47ea4dfec3ea3f8aa2331fd
IU-Studies <notes.in.drive.forever@gmail.com>
2024-09-27 17:44:15 +0530

    Create Write a program that prompts the user to enter two number and then prints out the result of the following operations: Addition, Substraction, Multiplication, Division, Modulus, Exponentiation and bitwise AND, OR, XOR and NOT.py

---
+++ Python/Write a program that prompts the user to enter two number and then prints out the result of the following operations: Addition, Substraction, Multiplication, Division, Modulus, Exponentiation and bitwise AND, OR, XOR and NOT.py
@@ -0,0 +1,15 @@
+#Write a program that prompts the user to enter two number and then prints out the result of the following operations: Addition, Substraction, Multiplication, Division, Modulus, Exponentiation and bitwise AND, OR, XOR and NOT.
+
+num1 = int(input("Enter number one:-"))
+num2 = int(input("Enter number two:-"))
+print ("Addition of your two numbers are:-",num1+num2)
+print ("Substraction of your two numbers are:-",num1-num2)
+print ("Multiplication of your two numbers are:-",num1*num2)
+print ("Division of your two numbers are:-",num1/num2)
+print ("Modulus of your two numbers are:-",num1%num2)
+print ("Exponentiation of your two numbers are:-",num1**num2)
+print ("Bitwise AND Calculation of your two numbers are:-",num1&num2)
+print ("Bitwise OR Calculation your two numbers are:-",num1|num2)
+print ("Bitwise XOR Calculation your two numbers are:-",num1^num2)
+print ("Bitwise NOT Calculation of your two numbers are:-",~num1+1)
+print ("Bitwise NOT Calculation of your two numbers are:-",~num2+1)
