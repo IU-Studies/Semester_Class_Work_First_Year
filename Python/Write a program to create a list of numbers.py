ce128fc25b28cf4829006325ac6950b586dec867
IU-Studies <notes.in.drive.forever@gmail.com>
2024-11-13 18:08:41 +0530

    Create Write a program to create a list of numbers.py

---
+++ Python/Write a program to create a list of numbers.py
@@ -0,0 +1,6 @@
+lst = []
+resp = int(input("How many numbers do you want to enter?: "))
+for i in range(resp):
+    user = input("Enter a number to add to the list: ")
+    lst.append(user)
+print(lst)
