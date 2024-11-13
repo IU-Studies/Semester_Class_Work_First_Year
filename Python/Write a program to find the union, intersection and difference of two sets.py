a2e0b72dfcd05156d1768ab20f8b15e52ad0c0ca
IU-Studies <notes.in.drive.forever@gmail.com>
2024-11-13 18:23:03 +0530

    Create Write a program to find the union, intersection and difference of two sets.py

---
+++ Python/Write a program to find the union, intersection and difference of two sets.py
@@ -0,0 +1,6 @@
+set1 = eval(input("Enter your first set with elements:-"))
+set2 = eval(input("Enter your second set with elements:-"))
+
+print("The union of two sets are:-",set1.union(set2))
+print("The intersection of two sets are:-",set1.intersection(set2))
+print("The difference of two sets are:-",set1.difference(set2))
